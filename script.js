108b9c0f3fa29d42545ea3afa25a8126d1719c0a
unknown <yoncelita>
2023-04-30 00:07:42 +0300

    adding functionallity to the checkbox

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function dataFill() {
 
     data.map((values) => {
         tableBody += `<tr>
-        <td>${values.id}</td>
+        <td><input type="checkbox" id="checker" name="checker"></td>
         <td>${values.name}</td>
         <td>${values.username}</td>
         <td>${values.email}</td>
@@ -19,5 +19,27 @@ async function dataFill() {
 
     document.getElementById("tableBody").innerHTML = tableBody;
 
+
 }
+
 dataFill()
+
+const parentElement = document.getElementById("tableBody");
+parentElement.addEventListener("click", (event) => {
+    // Check if event was triggered by a checkbox
+    if (event.target.type === "checkbox") {
+        // Check if checkbox is selected
+        if (event.target.checked) {
+            console.log("Checkbox is selected");
+            let checkedRow = event.target.parentElement.parentElement;
+            checkedRow.style.backgroundColor = 'var(--green)';
+            checkedRow.style.color = 'var(--white)';
+
+        } else {
+            console.log("Checkbox is not selected");
+            let checkedRow = event.target.parentElement.parentElement;
+            checkedRow.style.backgroundColor = 'var(--white)';
+            checkedRow.style.color = 'var(--primary)';
+        }
+    }
+});
